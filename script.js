f38d525783844141cdbb755846cfcfb613b83b61
Arianna Choza <unachoza@gmail.com>
2019-01-28 10:48:32 -0800

    toggling innerHTML

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-alert('working')
 
+const text = document.querySelector('h1')
+
+const thisButton = document.querySelector('.button')
+thisButton.addEventListener("click", function(){
+    console.log("i was clicked")
+    if(text.innerHTML === "damn right") {
+        text.innerHTML = "This website will be beautiful"
+    } else{
+        text.innerHTML = "damn right"
+    }
+})
